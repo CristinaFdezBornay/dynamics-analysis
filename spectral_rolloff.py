5e532a28039adc6fc4017b303b509cde6eb0f57e
Cristina Fernández Bornay <cristinafernandezbornay@outlook.com>
2021-04-01 19:13:23 +0200

    feat(spectral rolloff): working

---
+++ spectral_rolloff.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import librosa
+import librosa.display
+import sklearn
+
+audio_data = 'prueba_hard.wav'
+x , sr = librosa.load(audio_data, sr=48000)
+
+# Calculate spectral rolloff
+spectral_rolloff = librosa.feature.spectral_rolloff(x+0.01, sr=sr)[0]
+
+# Computing the time variable for visualization
+plt.figure(figsize=(12, 4))
+frames = range(len(spectral_rolloff))
+t = librosa.frames_to_time(frames)
+
+# Normalising the spectral rolloff for visualisation
+def normalize(x, axis=0):
+    return sklearn.preprocessing.minmax_scale(x, axis=axis)
+
+librosa.display.waveplot(x, sr=sr, alpha=0.4)
+plt.plot(t, normalize(spectral_rolloff), color='r')
+plt.xlabel("Time (s)")
+plt.title("Spectral Rolloff")
+plt.show()
